418994113e2983505e874f14602e9f299cc3a4e3
LeoXinhaoLee <Mc1114207918@outlook.com>
2025-11-04 04:02:03 +0000

    rm files

--- offline_timing/test_determ.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export CUDA_HOME=$CONDA_PREFIX
-
-# python3 -m sglang.test.test_deterministic --host '127.0.0.1' --port 30000 --n-trials 4 --test-mode single
-
-# python3 -m sglang.test.test_deterministic --host '127.0.0.1' --port 30000 --n-trials 16 --test-mode mixed
-
-python3 -m sglang.test.test_deterministic --host '127.0.0.1' --port 30000 --n-trials 50 --test-mode prefix
